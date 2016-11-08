47c14b0f04853920033881093d956b74d3e94c72
Matthew Crist <matthew.crist@boston.gov>
2016-11-08 14:34:39 -0500

    Update public notice and style the listing view

--- docroot/sites/all/themes/custom/boston/templates/views/views-view--public-notice-landing.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/views/views-view--public-notice-landing.tpl.php
@@ -7,65 +7,64 @@
  * @ingroup views_templates
  */
 ?>
-<div class="<?php print $classes; ?>">
-  <?php print render($title_prefix); ?>
-  <?php if ($title): ?>
-    <?php print $title; ?>
-  <?php endif; ?>
-  <?php print render($title_suffix); ?>
+<div class="b b--fw">
+  <div class="b-fv b--g">
+    <?php if ($exposed): ?>
+      <div class="view-filters">
+        <?php print $exposed; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>
+<div class="b b--g b--fw">
+  <div class="b-c">
+    <?php if ($attachment_before): ?>
+      <div class="attachment attachment-before">
+        <?php print $attachment_before; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($exposed): ?>
-    <div class="view-filters">
-      <?php print $exposed; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($header): ?>
+      <div class="view-header">
+        <?php print $header; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($attachment_before): ?>
-    <div class="attachment attachment-before">
-      <?php print $attachment_before; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($rows): ?>
+      <div class="view-content">
+        <?php print $rows; ?>
+      </div>
+    <?php elseif ($empty): ?>
+      <div class="view-empty">
+        <?php print $empty; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($header): ?>
-    <div class="view-header">
-      <?php print $header; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($pager): ?>
+      <?php print $pager; ?>
+    <?php endif; ?>
 
-  <?php if ($rows): ?>
-    <div class="view-content">
-      <?php print $rows; ?>
-    </div>
-  <?php elseif ($empty): ?>
-    <div class="view-empty">
-      <?php print $empty; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($attachment_after): ?>
+      <div class="attachment attachment-after">
+        <?php print $attachment_after; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($pager): ?>
-    <?php print $pager; ?>
-  <?php endif; ?>
+    <?php if ($more): ?>
+      <?php print $more; ?>
+    <?php endif; ?>
 
-  <?php if ($attachment_after): ?>
-    <div class="attachment attachment-after">
-      <?php print $attachment_after; ?>
-    </div>
-  <?php endif; ?>
+    <?php if ($footer): ?>
+      <div class="view-footer">
+        <?php print $footer; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($more): ?>
-    <?php print $more; ?>
-  <?php endif; ?>
+    <?php if ($feed_icon): ?>
+      <div class="feed-icon">
+        <?php print $feed_icon; ?>
+      </div>
+    <?php endif; ?>
 
-  <?php if ($footer): ?>
-    <div class="view-footer">
-      <?php print $footer; ?>
-    </div>
-  <?php endif; ?>
-
-  <?php if ($feed_icon): ?>
-    <div class="feed-icon">
-      <?php print $feed_icon; ?>
-    </div>
-  <?php endif; ?>
-
-</div><?php /* class view */ ?>
+  </div><?php /* class view */ ?>
+</div>
