9be985d6a0ff4758849151810357b410026ad751
Matthew Crist <matthew.crist@boston.gov>
2016-12-12 15:31:33 -0500

    Don't index stuff in header or main navigation

--- docroot/sites/all/themes/custom/boston/templates/default/region--footer.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/default/region--footer.tpl.php
@@ -9,7 +9,7 @@
  */
 ?>
 <?php if ($content): ?>
-  <footer class="footer <?php print $classes; ?>">
+  <footer class="footer <?php print $classes; ?>" data-swiftype-index="false">
     <div class="container">
       <a class="icon-311-link" href="http://www.cityofboston.gov/311/" target="_blank">
         <span class="icon-311-reportbos">BOS-311 - </span><span class="icon-311-report">Report an issue</span>
