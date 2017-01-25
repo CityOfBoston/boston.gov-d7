f3e53f670614f4798e682f3b8e9cdf0a5f973886
Matthew Crist <matthewcrist@users.noreply.github.com>
2017-01-25 10:19:51 -0500

    Revert "[RELEASE] 01/26/2017"

--- docroot/sites/all/themes/custom/boston/templates/field/field--field-intro-text--tabbed-content.tpl.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @file
- * Default implementation of field intro text on tabbed content nodes.
- */
-?>
-<div class="intro-text supporting-text">
-  <?php print render($items); ?>
-</div>
