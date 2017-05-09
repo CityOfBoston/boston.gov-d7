d3d9807d4c8da240dd5a91231e9686d5db8a18fd
Matthew Crist <matthew.crist@boston.gov>
2017-05-09 16:30:26 -0400

    More progress on emergency alert form

--- docroot/sites/all/themes/custom/boston/templates/component/paragraphs-item--bos-signup-emergency-alerts.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/component/paragraphs-item--bos-signup-emergency-alerts.tpl.php
@@ -16,42 +16,42 @@
       <div class="g--5">
         <form class="" action="#" method="post">
           <div class="fs">
-            <div class="txt">
-              <label for="text" class="txt-l txt-l--w m-t000">Your email address</label>
-              <input id="email" type="text" value="" placeholder="Email address" class="txt-f txt-f--fw">
+            <div class="fs-c m-b300">
+              <div class="txt">
+                <label for="email" class="txt-l txt-l--mt000">Your email address</label>
+                <input id="email" type="text" value="" placeholder="email@address.com" class="txt-f txt-f--sm">
+              </div>
+              <div class="txt">
+                <label for="phone_number" class="txt-l txt-l--w txt-l--mt000">Your phone number</label>
+                <input id="phone_number" type="text" value="" placeholder="Phone number" class="txt-f txt-f--sm">
+              </div>
             </div>
-            <div class="txt">
-              <label for="text" class="txt-l txt-l--w m-t000">Your phone number</label>
-              <input id="phone_number" type="text" value="" placeholder="Phone number" class="txt-f txt-f--fw">
-            </div>
-            <div class="txt">
-              <label class="cb" for="checkbox-call">
+            <div class="fs-c fs-c--i">
+              <label class="cb">
                 <input id="checkbox-call" name="checkbox-call" type="checkbox" value="public_notices" class="cb-f">
-                <span class="cb-l">Call me</span>
+                <span class="cb-l cb-l--sans">Call me</span>
               </label>
-              <label class="cb" for="checkbox-">
+              <label class="cb">
                 <input id="checkbox-text" name="checkbox-text" type="checkbox" value="public_notices" class="cb-f">
-                <span class="cb-l">Text me</span>
+                <span class="cb-l cb-l--sans">Text me</span>
               </label>
             </div>
-            <hr />
-            <div class="g">
-              <div class="g--6">
-                <div class="txt">
-                  <label for="first_name" class="txt-l txt-l--w m-t000">First name</label>
-                  <input id="first_name" type="first_name" value="" placeholder="First name" class="txt-f txt-f--fw">
-                </div>
+            <hr class="hr hr--sq" />
+            <div class="fs-c m-b300">
+              <div class="txt">
+                <label for="first_name" class="txt-l txt-l--mt000">First name</label>
+                <input id="first_name" type="text" value="" placeholder="First name" class="txt-f txt-f--sm">
               </div>
-              <div class="g--6">
-                <div class="txt">
-                  <label for="last_name" class="txt-l txt-l--w m-t000">Last name</label>
-                  <input id="last_name" type="text" value="" placeholder="Last name" class="txt-f txt-f--fw">
-                </div>
+              <div class="txt">
+                <label for="last_name" class="txt-l txt-l--mt000">Last name</label>
+                <input id="last_name" type="text" value="" placeholder="Last name" class="txt-f txt-f--sm">
               </div>
             </div>
-            <div class="txt">
-              <label for="zip_code" class="txt-l txt-l--w m-t000">Your phone number</label>
-              <input id="zip_code" type="text" value="" placeholder="Zip code" class="txt-f" size="10">
+            <div class="fs-c m-b300">
+              <div class="txt">
+                <label for="zip_code" class="txt-l txt-l--w txt-l--mt000">Your phone number</label>
+                <input id="zip_code" type="text" value="" placeholder="Zip code" class="txt-f txt-f--sm" size="10">
+              </div>
             </div>
             <div class="bc bc--r">
               <button type="submit" class="btn btn--700">Sign Up</button>
