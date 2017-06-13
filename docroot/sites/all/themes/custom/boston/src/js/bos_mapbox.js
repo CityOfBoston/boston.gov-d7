d06f9d4a581f42dcf81912b0bebf81fbfc921643
jimafisk <jim.fisk@jantcu.com>
2017-06-13 18:41:53 +0000

    Remove filters and related js.

--- docroot/sites/all/themes/custom/boston/src/js/bos_mapbox.js
+++ docroot/sites/all/themes/custom/boston/src/js/bos_mapbox.js
@@ -46,69 +46,6 @@
       food_trucks.bindPopup(function (layer) {
         return L.Util.template('<a class="title" href={Link} target="_blank"><b>{Truck}</b></a><p class="times">{Time}: {Hours}<br>Day: {Day}<br><br>{Title}</p><p class="content">{Loc}<br><br>Managed by: {Management}</p>', layer.feature.properties);
       });
-      // add day, time of day, and truck filters
-      // get day of week as a string
-      function DayOfWeekAsString(dayIndex) {
-        return ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dayIndex];
-      }
-      // get current day of week and make the default selection
-      var d = new Date();
-      var n = d.getDay(); 
-      var temp = DayOfWeekAsString(n);
-      var day = document.getElementById('day');
-      var days = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-      day.selectedIndex = days.indexOf(temp);
-      day.addEventListener('change', function(){
-        food_trucks.setWhere(day.value + " AND " + time.value + " AND " + truck.value);
-      });
-      // build time of day filter
-      // get current time of day as string value
-      function GetTimeIndex(time) {
-        if (time >= '07:00:00' && time <= '10:30:00') {
-          return "Breakfast";
-        } else if (time <= '15:00:00')  {
-          return "Lunch";
-        } else if (time <= '20:00:00') {
-          return "Dinner";
-        } else {
-          return "Any";
-        }}
-      // get current time of day
-      var t = GetTimeIndex(d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));
-      console.log(d.toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1"));
-      console.log(t);
-      var time = document.getElementById('time');
-      var times = ["Any", "Breakfast", "Lunch", "Dinner"];
-      time.selectedIndex = times.indexOf(t);
-      time.addEventListener('change', function(){
-        food_trucks.setWhere(day.value + " AND " + time.value + " AND " + truck.value);
-      });
-      // build trucks fitler
-      // query trucks layer to get unique names of trucks
-      var trucksQuery = L.esri.query({
-        url: "https://services.arcgis.com/sFnw0xNflSi8J0uh/arcgis/rest/services/food_trucks_schedule/FeatureServer/0"
-      })
-        .where("1=1")
-        .returnGeometry(false)
-        .fields(['Truck']);
-      trucksQuery.params.returnDistinctValues = true;
-      // run query and build options list from it
-      trucksQuery.run(function (err, res, raw) {
-        for (i=1; i<res.features.length; i++) {
-          var option = document.createElement("option");
-          option.text = res.features[i].properties.Truck;
-          option.value = "Truck = '" + res.features[i].properties.Truck + "'";
-          var select = document.getElementById("truck");
-          select.appendChild(option);
-        }
-      });
-      truck.addEventListener('change', function(){
-        food_trucks.setWhere(day.value + " AND " + time.value + " AND " + truck.value);
-      });
-      // once all food trucks have loaded adjust display to the default selection
-      food_trucks.once('load', function(){
-        food_trucks.setWhere(day.value + " AND " + time.value + " AND " + truck.value);
-      });
 
     }
   };
