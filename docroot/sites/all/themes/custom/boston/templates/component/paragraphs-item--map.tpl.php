5b78d9da22211f2f7944804f971f065405ee90eb
jimafisk <jim.fisk@jantcu.com>
2017-06-30 14:19:27 +0000

    Remove #markup key that was drawing canvas a second time.

--- docroot/sites/all/themes/custom/boston/templates/component/paragraphs-item--map.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/component/paragraphs-item--map.tpl.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @file
  * Default theme implementation for a single paragraph item.
@@ -43,7 +44,6 @@
           </div>
         </div>
       <?php endif; ?>
-      <!-- <div id="map1" class="map"></div> MAP CANVAS IS CREATED IN PREPROCESS-->
       <?php print render($content['map_canvas']) ?>
     </div>
   </div>
@@ -56,105 +56,105 @@
 body { margin:0; padding:0; }
 .map { position: absolute; top:0; bottom:0; right:0; left:0; z-index:0; }
 .leaflet-popup {
-	width: 250px;
-	height: auto;
+  width: 250px;
+  height: auto;
 }
 .leaflet-popup-content .title {
-	font-size: 22px;
-	font-family: 'Montserrat', sans-serif;
-	font-weight: bold;
+  font-size: 22px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: bold;
 }
 .leaflet-popup-content .times {
-	font-size: 16px;
-	font-family: 'Lora', serif;
-	font-style: italic;
+  font-size: 16px;
+  font-family: 'Lora', serif;
+  font-style: italic;
 }
 .leaflet-popup-content .content {
-	font-size: 14px;
-	font-family: 'Lora', serif;
-	font-style: italic;
+  font-size: 14px;
+  font-family: 'Lora', serif;
+  font-style: italic;
 }
 .leaflet-popup-content p {
-	font-size: 14px;
-	font-family: 'Lora', serif;
-	margin: 0px;
+  font-size: 14px;
+  font-family: 'Lora', serif;
+  margin: 0px;
 }
 a[href=""] {
-	color: #000000;
-	text-decoration: none;
-	cursor: default;
+  color: #000000;
+  text-decoration: none;
+  cursor: default;
 }
 
 .queries {
-	position: static;
-	top: 25px;
-	left: 25px;
-	background: white;
-	z-index: 1000;
-	/*border-radius: 10px;
-	box-shadow: 5px 5px 5px #888888;*/
+  position: static;
+  top: 25px;
+  left: 25px;
+  background: white;
+  z-index: 1000;
+  /*border-radius: 10px;
+  box-shadow: 5px 5px 5px #888888;*/
 }
 @media screen and (min-width: 980px) {
-	.queries {
-		position: absolute;
-		padding: .5em;
-	}
+  .queries {
+    position: absolute;
+    padding: .5em;
+  }
 }
 .query {
-	padding: .5em;
-	box-shadow: none;
+  padding: .5em;
+  box-shadow: none;
 }
 label {
-	font-size: 22px;
-	font-family: "Montserrat", Arial, sans-serif;
-	font-weight: bold;
+  font-size: 22px;
+  font-family: "Montserrat", Arial, sans-serif;
+  font-weight: bold;
 }
 .query select {
-	font-family: 'Lora', serif;
-	font-size: 18px;
-	font-style: italic;
+  font-family: 'Lora', serif;
+  font-size: 18px;
+  font-style: italic;
 }
 /* style leaflet marker clusters */
 .marker-cluster-small {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster-small div {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster-medium {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster-medium div {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster-large {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster-large div {
-	background-color: rgba(40, 139, 228, 0.6);
+  background-color: rgba(40, 139, 228, 0.6);
 }
 .marker-cluster div {
-	width: 30px;
-	height: 30px;
-	margin-left: 5px;
-	margin-top: 5px;
-	text-align: center;
-	border-radius: 15px;
-	font-family: 'Montserrat', sans-serif;
-	font-weight: 500;
-	font-size: : 12px;
-	color: white;
+  width: 30px;
+  height: 30px;
+  margin-left: 5px;
+  margin-top: 5px;
+  text-align: center;
+  border-radius: 15px;
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+  font-size: : 12px;
+  color: white;
 }
 /* Style Legend */
 .legend {
-	line-height: 18px;
-	color: #555;
-	background-color: rgba(255, 250, 250, 1);
-	border: 3px;
-	outline-color: white;
-	border-radius: 5px;
-	padding: 6px 8px;
-	font-size: 14px;
+  line-height: 18px;
+  color: #555;
+  background-color: rgba(255, 250, 250, 1);
+  border: 3px;
+  outline-color: white;
+  border-radius: 5px;
+  padding: 6px 8px;
+  font-size: 14px;
 }
 .legend i {
   width: 18px;
