af44a505b174c92fb0f685375277af47a2df1a27
jimafisk <jim.fisk@jantcu.com>
2017-07-12 19:14:17 +0000

    Assign css state classes to individual drawers.

--- docroot/sites/all/themes/custom/boston/src/js/drawer.js
+++ docroot/sites/all/themes/custom/boston/src/js/drawer.js
@@ -48,20 +48,46 @@
     });
   }
 
-  // Expand drawers when user searchs page with CTRL+f
-  var expanded;
+  // Expand drawers so users can search within them easily.
   $(document).on("keydown", function (e) {
-    if (e.keyCode == 70 && e.ctrlKey && !expanded) {
-      $('.dr-h').click();
-      expanded = true;
+    // Check for CTRL+f input.
+    if (e.keyCode == 70 && e.ctrlKey) {
+      // Loop through each label that controls a hidden checkbox.
+      $('.dr-h').each(function() {
+        // Only execute on drawers that are closed.
+        if (!$(this).hasClass('expanded')) {
+          // Simulate a click on the drawer label to open it.
+          $(this).click();
+          // Add a class so we know this drawer is now open.
+          $(this).addClass('expanded');
+        }
+      });
     }
-    if (e.keyCode == 27 && expanded) {
-      $('.dr-tr').attr('checked', false);
-      expanded = false;
+    // Check for ESC input.
+    if (e.keyCode == 27) {
+      // Loop through each label that controls a hidden checkbox.
+      $('.dr-h').each(function() {
+        // Only execute on drawers that are open.
+        if ($(this).hasClass('expanded')) {
+          // Simulate a click on the drawer label to close it.
+          $(this).click();
+          // Remove a class so we know this drawer is now closed.
+          $(this).removeClass('expanded');
+        }
+      });
     }
   });
-  $('label.dr-h').click(function() {
-    expanded = false;
+  // Check for clicks outside of browser search.
+  $('.dr-h').click(function() {
+    // Check if the clicked drawer is already open.
+    if ($(this).hasClass('expanded')) {
+      // Remove the class so we know this drawer was closed manually.
+      $(this).removeClass('expanded');
+    // If the drawer is closed already.
+    } else {
+      // Add a class so we know if was just opened.
+      $(this).addClass('expanded');
+    }
   });
 
 })(jQuery, Drupal, this, this.document);
