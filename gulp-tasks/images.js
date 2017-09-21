7a9e1eb03a0fe2b6dd0df900df69c3cc4e457a4b
Matthew Crist <matthew.crist@boston.gov>
2017-09-21 09:34:25 -0400

    Clean up comments to make build happy

--- gulp-tasks/images.js
+++ gulp-tasks/images.js
@@ -7,7 +7,6 @@
 module.exports = function (gulp, plugins, options, util) {
   return function () {
     var stream = gulp.src([
-                       options.boston + '/src/img/**/*.{png,jpg,jpeg,gif,svg}',
                        options.images + '/**/*.{png,jpg,jpeg,gif,svg}'
                      ])
                      .pipe(gulp.dest(options.dest + '/img'));
