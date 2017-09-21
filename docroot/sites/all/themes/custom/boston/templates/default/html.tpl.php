8ef4dc9fd8a2f8ae3648e1d79d4ca05783ab36ef
Matthew Crist <matthew.crist@boston.gov>
2017-09-21 12:22:10 -0400

    Remove Google fonts from repo

--- docroot/sites/all/themes/custom/boston/templates/default/html.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/default/html.tpl.php
@@ -11,8 +11,6 @@
     })(window,document,'script','dataLayer','<?php echo $google_tag_manager_id ?>');</script>
   <?php } ?>
 
-  <link href='https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic|Montserrat:400,700' rel='stylesheet' type='text/css'>
-
   <!--[if !IE]><!-->
 	<link href='<?php print $asset_url ?>/css/<?php print $asset_name ?>.css?<?php print $cache_buster ?>' rel='stylesheet' type='text/css'>
   <link href='<?php print $asset_url ?>/legacy/<?php print $asset_name ?>.css?<?php print $cache_buster ?>' rel='stylesheet' type='text/css'>
