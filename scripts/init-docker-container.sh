6312abde128914e7c26f755f3fc3a4f0465d7666
Fin Hopkins <fin.hopkins@boston.gov>
2018-01-19 16:52:20 -0500

    Adds a Dockerfile and docker-compose file
    First pass at starting up a blank Boston.gov install with a few
    repeatable commands.
    
    Major change is to run the "update" functions inside the hook_installs
    for the bos_profile and bos_core. This is based on the assumption that
    upgrading in production will not run hook_install.
    
    Currently only tested for Boston.gov, not for The Hub.

---
+++ scripts/init-docker-container.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -e
+set -x
+
+# We want to symlink specific directories from the host into
+# the Apache docroot. This speeds things up by keeping the
+# vendor files just within the container. Pulling files across
+# the bind volume is slow in comparison.
+paths=(
+  docroot/profiles
+  docroot/sites/default
+  docroot/sites/hub
+  docroot/sites/all/modules/custom
+  docroot/sites/all/modules/features
+  docroot/sites/all/settings
+  docroot/sites/all/themes/custom
+)
+
+for p in "${paths[@]}"
+do
+  rm -rf "./${p}"
+  ln -s "/host-repo/${p}" "./${p}"
+done
+
+if [ "$1" = "hub" ]
+then
+  ./hub-task.sh -Dproject.build_db_from=initialize build:local
+else
+  ./task.sh -Dproject.build_db_from=initialize build:local
+  chown www-data /boston.gov/docroot/sites/default/files 
+fi
