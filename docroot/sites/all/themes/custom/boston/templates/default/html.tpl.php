7daacf7627185d2c3a3d8b347f961aafa27094ca
Matthew McGowan <matthew.mcgowan@boston.gov>
2019-02-07 16:58:26 -0500

    - Added anti flicker snippet for Google Tag Manager / Optimizer.

--- docroot/sites/all/themes/custom/boston/templates/default/html.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/default/html.tpl.php
@@ -4,6 +4,15 @@
   <title><?php print $head_title; ?></title>
 
   <?php if ($google_tag_manager_id) { ?>
+    <!-- Anti-flicker GTAG code snippet -->
+    <style>.async-hide { opacity: 0 !important} </style>
+    <script>(function(a,s,y,n,c,h,i,d,e){s.className+=' '+y;h.start=1*new Date;
+    h.end=i=function(){s.className=s.className.replace(RegExp(' ?'+y),'')};
+    (a[n]=a[n]||[]).hide=h;setTimeout(function(){i();h.end=null},c);h.timeout=c;
+    })(window,document.documentElement,'async-hide','dataLayer',4000,
+    {'<?php echo $google_tag_manager_id ?>':true});</script>
+    
+    <!-- GTAG code snippet -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
