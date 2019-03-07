4286550927e97c10a5534b185f49b4da98884c39
David Upton <davidrkupton@users.noreply.github.com>
2019-03-07 13:26:03 -0500

    Update task.sh

--- task.sh
+++ task.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-
 if [[ ! -f ${DIR}/vendor/bin/phing ]]; then
    echo "Phing was not found in this project's bin directory."
    echo "Please run composer install."
