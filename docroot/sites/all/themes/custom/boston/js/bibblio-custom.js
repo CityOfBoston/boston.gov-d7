fe0afa78095707e785346b624bcd803f2f7e0dd7
Matthew McGowan <matthew.mcgowan@boston.gov>
2019-03-26 15:49:41 -0400

    - Updated condition, and added function to check/find if Bibblio specific Bibblio image exists

--- docroot/sites/all/themes/custom/boston/js/bibblio-custom.js
+++ docroot/sites/all/themes/custom/boston/js/bibblio-custom.js
@@ -1,47 +1,61 @@
- var bibblioImgObj = [
+ var imgBase = 'https://boston.gov/';
+ var randImgObj = [
     {   'desc' : 'skyline',
-        'path' : 'sites/default/files/styles/rep_wide_2000x700custom_boston_desktop_2x/public/hero-image-03-2019/boston-skyline.jpg'
+        'path' : imgBase + 'sites/default/files/styles/rep_wide_2000x700custom_boston_desktop_2x/public/hero-image-03-2019/boston-skyline.jpg'
     },{
         'desc' : 'boston_common',
-        'path' : 'sites/default/files/styles/resp_wide_2000x460custom_boston_desktop_1x/public/photo-image-09-2018/dsc_0134_1.jpg'
+        'path' : imgBase + 'sites/default/files/styles/resp_wide_2000x460custom_boston_desktop_1x/public/photo-image-09-2018/dsc_0134_1.jpg'
     },{
         'desc' : 'state_house',
-        'path' : 'sites/default/files/styles/featured_item_thumbnail/public/statehouse.jpg'
+        'path' : imgBase + 'sites/default/files/styles/featured_item_thumbnail/public/statehouse.jpg'
     },{
         'desc' : 'nh-roslindale',
-        'path' : 'sites/default/files/styles/grid_card_image/public/roslindale.jpg'
+        'path' : imgBase + 'sites/default/files/styles/grid_card_image/public/roslindale.jpg'
     },{
         'desc' : 'nh-jamaica_plain',
-        'path' : 'sites/default/files/styles/grid_card_image/public/jamaicaplain4.jpg'
+        'path' : imgBase + 'sites/default/files/styles/grid_card_image/public/jamaicaplain4.jpg'
     },{
         'desc' : 'nh-back_bay',
-        'path' : 'sites/default/files/styles/grid_card_image/public/backbay5.jpg'
+        'path' : imgBase + 'sites/default/files/styles/grid_card_image/public/backbay5.jpg'
     }
 ]
-var getImg = function () {
-    num = Math.floor(Math.random() * (bibblioImgObj.length - 1));
-    findItem = bibblioImgObj.findIndex(i => i.desc === bibblioImgObj[num].desc); 
+var getImgRand = function () {
+    num = Math.floor(Math.random() * (randImgObj.length - 1));
+    findItem = randImgObj.findIndex(i => i.desc === randImgObj[num].desc); 
     //console.log(findItem +' : '+bibblioImgObj[findItem].path);
-    pathVal = bibblioImgObj[findItem];
-    bibblioImgObj.splice(findItem,1);
-    return pathVal;
+    pathVal = randImgObj[findItem];
+    randImgObj.splice(findItem,1);
+    return  pathVal;
+}
+var getImgBibblio = function (infoData) {
+    let imgObj = {
+        'desc' : infoData.description,
+        'path' : infoData.image.contentUrl
+        }
+    return imgObj;
 }
 var getHTML = function(bibContent){
   var listItem = '';
   jQuery(bibContent).each(function(index,value){
       let bibFields = value.fields;
-      let imgInfo = getImg();
+      let imgInfo;
+      let checkImg = bibFields.image;
+      //console.log(bibFields);
+      if(checkImg == null){
+        imgInfo = getImgRand() 
+      }else{
+        imgInfo = getImgBibblio(bibFields)
+      }
       let bibName = bibFields.name;
       let bibUrl = bibFields.url;
       let bibDesc = bibFields.description; 
-      listItem += '<a class= "cd g--4 g--4--sl m-t500 bibblio" bibblio-title="'+bibName+'" bibblio-img-desc="'+imgInfo.desc+'" href="'+bibUrl+'"><div class="cd-ic" style="background-image:url(https://boston.gov/'+imgInfo.path+')" ><\/div><div class="cd-c"><div class="cd-t">'+bibName+'<\/div><div class="cd-d"\>'+bibDesc+'<\/div><\/div><\/a>';
+      listItem += '<a class= "cd g--4 g--4--sl m-t500 bibblio" bibblio-title="'+bibName+'" bibblio-img-desc="'+imgInfo.desc+'" href="'+bibUrl+'"><div class="cd-ic" style="background-image:url('+ imgInfo.path +')" ><\/div><div class="cd-c"><div class="cd-t">'+bibName+'<\/div><div class="cd-d"\>'+bibDesc+'<\/div><\/div><\/a>';
       
   });
   jQuery('#bibblio-custom div.g').append(listItem);
 }
 const pageURL = window.location.pathname;
 const siteLocation = 'https://www.boston.gov';
-//const siteLocation = 'https://bostonuat.prod.acquia-sites.com';
 jQuery.ajax({
   method: "GET",
   url: "https://api.bibblio.org/v1/recommendations",
@@ -54,7 +68,7 @@ jQuery.ajax({
   },
   data:{ 
     "customUniqueIdentifier": siteLocation + pageURL,
-    "fields":"name,moduleImage,url,datePublished,description", 
+    "fields":"name,image,image,url,datePublished,description", 
     "limit":"3",
   },
   success: function (res){
