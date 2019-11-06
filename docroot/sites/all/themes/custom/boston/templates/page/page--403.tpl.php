15fd3449e3899596d816217864bc3ae3188040d0
Matthew McGowan <matthew.mcgowan@boston.gov>
2019-11-06 12:07:28 -0500

    Update for access boston and hub 403 page language / functionality.

--- docroot/sites/all/themes/custom/boston/templates/page/page--403.tpl.php
+++ docroot/sites/all/themes/custom/boston/templates/page/page--403.tpl.php
@@ -9,13 +9,22 @@
  * @see https://drupal.org/node/1728148
  */
 ?>
+<?php 
+	$sso_link = variable_get('sso_login_url', FALSE);
+?>
 <div class="main err err--403">
   <?php include drupal_get_path('theme', 'boston') . '/templates/snippets/header.tpl.php'; ?>
   <div class="container">
     <section class="main-content err-content" id="content" role="main">
       <div class="denied-text error-text column desktop-66-left">
-        <h1><?php print $title; ?></h1>
-        <?php print token_replace('[boston:403-page-text]'); ?>
+        <h1>login required</h1>
+        <?php //print token_replace('[boston:403-page-text]'); ?>
+        <p><em>Please log in through the button below:</em></p>
+        <?php if($sso_link) : ?>
+            <a href="<?php echo $sso_link; ?>" title="Login" class="button">Login</a>
+        <?php else:  ?>
+            <a href="/user?local" title="Login" class="button">Login</a>
+        <?php endif; ?>
       </div>
     </section>
   </div>
